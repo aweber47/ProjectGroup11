8a2a4a00bcfdbd663c00dbc84e9b4ad0e7a882b7
J Rit <jgritterjr@gmail.com>
2022-03-31 14:06:18 -0400

    prep work

--- index.php
+++ index.php
@@ -4,5 +4,24 @@
  * File: index.php*
  * Description: */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Title Goes Here</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+        
+        </header>
+        <section>
+        
+        </section>
+        <footer>
+        
+        </footer>
+    </body>
+</html>
 <h2>We are all screwed!</h2>
 <p>We might have something now.</p>
