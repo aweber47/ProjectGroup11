a3be43a7a5efb602b102ce3ed35186b32d89b2d7
AlexWeber <aw118@iu.edu>
2022-04-14 14:08:25 -0400

    added styles to the confirmation page for menu item

--- views/menu/menu_index_view.class.php
+++ views/menu/menu_index_view.class.php
@@ -4,8 +4,10 @@
  * File: menu_index_view.class.php*
  * Description: displays the menu, includes honor features such as paginator, advanced search and more to come!*/
 
-class MenuIndexView extends IndexView{
-    public static function displayHeader($title){
+class MenuIndexView extends IndexView
+{
+    public static function displayHeader($title)
+    {
         parent::displayHeader($title)
         ?>
 
@@ -13,12 +15,13 @@ class MenuIndexView extends IndexView{
         <script xmlns="http://www.w3.org/1999/html">
             var media = 'menuItem';
         </script>
-        
+
         <!--create the search bar -->
         <div id="searchbar">
             <form id="searchbar-form" method="get" action="<?= BASE_URL ?>/menu/search/">
                 <label id="searchtextbox" for="searchtextbox"></label>
-                    <input type="search" name="query-terms" id="searchtextbox" placeholder="Search Menu" autocomplete="off" onkeyup="handleKeyUp(event)">
+                <input type="search" name="query-terms" id="searchtextbox" placeholder="Search Menu" autocomplete="off"
+                       onkeyup="handleKeyUp(event)">
 
                 <input id="search-button" type="submit" value="Search">
 
@@ -28,37 +31,37 @@ class MenuIndexView extends IndexView{
          *         Below are honor project Implementations for search features               *
          ************************************************************************************/
         ?>
-        
+
         <div id="advanced-features">
             <a onclick="toggleText()">[ Advanced Search Features ]<br>WIP</a>
         </div>
-        
+
         <!-- This div determines if the other elements are shown.-->
         <!-- revert to searchbar styles -->
         <div id="Myid" hidden>
             <div id="pagination">
                 <!-- AND SEARCH -->
-                    <input checked type="radio" name="w" value="true">
-                        <label>Find all of my search terms (AND)</label>
+                <input checked type="radio" name="w" value="true">
+                <label>Find all of my search terms (AND)</label>
 
-                    <!-- OR SEARCH -->
-                    <input type="radio" name="w" value="false">
-                        <label>Find all of my search terms (OR)</label>
+                <!-- OR SEARCH -->
+                <input type="radio" name="w" value="false">
+                <label>Find all of my search terms (OR)</label>
                 <?php
                 /*************************************************************************************
                  *         Limiting Search by Attribute [product, price or description]               *
                  ************************************************************************************/
                 ?>
-                    <br><br>
-                    <!---Limiting the search more!-->
-                    <input checked type="checkbox" name="bool1" value="1"<?= (isset($_GET['bool1']) ? ' checked ' : '') ?>>
-                        <label>Product Name </label>
-    
-                    <input type="checkbox" name="bool2" value="2"<?= (isset($_GET['bool2']) ? ' checked ' : '') ?>>
-                        <label> Price </label>
-    
-                    <input type="checkbox" name="bool3" value="3"<?= (isset($_GET['bool3']) ? ' checked ' : '') ?>>
-                        <label>Description</label>
+                <br><br>
+                <!---Limiting the search more!-->
+                <input checked type="checkbox" name="bool1" value="1"<?= (isset($_GET['bool1']) ? ' checked ' : '') ?>>
+                <label>Product Name </label>
+
+                <input type="checkbox" name="bool2" value="2"<?= (isset($_GET['bool2']) ? ' checked ' : '') ?>>
+                <label> Price </label>
+
+                <input type="checkbox" name="bool3" value="3"<?= (isset($_GET['bool3']) ? ' checked ' : '') ?>>
+                <label>Description</label>
                 </form>
                 <?php
                 /*************************************************************************************
@@ -68,37 +71,36 @@ class MenuIndexView extends IndexView{
                 <form action="<?= BASE_URL ?>/menu/index/" method="post">
                     <!--- Limiting search by cat-->
                     <input type="radio" name="cat" value="0" onchange="this.form.submit()">
-                        <label>All Items</label>
+                    <label>All Items</label>
 
                     <input type="radio" name="cat" value="1" onchange="this.form.submit()">
-                        <label>Appetizers</label>
+                    <label>Appetizers</label>
 
                     <input type="radio" name="cat" value="2" onchange="this.form.submit()">
-                        <label>Entrees</label>
+                    <label>Entrees</label>
 
                     <input type="radio" name="cat" value="3" onchange="this.form.submit()">
-                        <label>Soups</label>
+                    <label>Soups</label>
                 </form>
-
-                <?php
-                /*************************************************************************************
-                 *         The Paginator  - -  -  -  -  -  -  -  -  - Yay :) -   -  -  -             *
-                 ************************************************************************************/
-                ?>
-
-                <form id="per-page-form" action="<?= BASE_URL ?>/menu/index/" method="post">
-                    <h3 id="searchbar">Items Per Page</h3>
-                    <input type="radio" name="items" value="3" onchange="this.form.submit()">
-                        <label>3 Products</label>
-
-                    <input type="radio" name="items" value="5" onchange="this.form.submit()">
-                        <label>5 Products</label>
-
-                    <input type="radio" name="items" value="25" onchange="this.form.submit()">
-                        <label>All Products</label>
-                </form>
-                <!-- This div determines if the other elements are shown.-->
             </div>
+        </div>
+        <?php
+        /*************************************************************************************
+         *         The Paginator  - -  -  -  -  -  -  -  -  - Yay :) -   -  -  -             *
+         ************************************************************************************/
+        ?>
+        <div id="pagination">
+            <form id="per-page-form" action="<?= BASE_URL ?>/menu/index/" method="post">
+                <h3 id="searchbar">Items Per Page</h3>
+                <input type="radio" name="items" value="3" onchange="this.form.submit()">
+                <label>3 Products</label>
+
+                <input type="radio" name="items" value="5" onchange="this.form.submit()">
+                <label>5 Products</label>
+
+                <input type="radio" name="items" value="25" onchange="this.form.submit()">
+                <label>All Products</label>
+            </form>
         </div>
         <!-- Javascript goes here -->
         <script type="text/javascript">
@@ -111,12 +113,13 @@ class MenuIndexView extends IndexView{
                 }
             }
         </script>
-        
+
         <div id="suggestionDiv"></div>
         <?php
     }
 
-    public static function displayFooter(){
+    public static function displayFooter()
+    {
         parent::displayFooter();
     }
 }
--- views/menu/verify/menu_verify.class.php
+++ views/menu/verify/menu_verify.class.php
@@ -13,11 +13,11 @@ class MenuVerify extends MenuIndexView{
         <!--<div id="main-header">Verification</div>-->
         
         <br>
-        
+        <div style="border: 1px solid black; margin: auto; padding: 10px; text-align: center; background-color: rgba(255, 215, 0, 0.85)">
         <table>
             <tr>
                 <td style="text-align: left; vertical-align: top;">
-                    <div>
+                    <div style="background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)">
                         <?= urldecode($message) ?>
                     </div>
                     <br>
@@ -26,8 +26,10 @@ class MenuVerify extends MenuIndexView{
         </table>
         
         <br><br><br><br>
-        
-        <a href="<?= BASE_URL ?>/menu/index">Back to menu list</a>
+        <div id="button-group">
+            <input class="edit-buttons" type="button" value="Back to Menu" onclick='window.location.href = "<?= BASE_URL . "/menu/index/" ?>"'>
+        </div>
+        </div>
         <?php
         //display footer
         parent::displayFooter();
