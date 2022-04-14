bc0077f57907e86d0a7bfbbb6513dab1662358a9
AlexWeber <aw118@iu.edu>
2022-04-14 14:12:31 -0400

    added styles for James :)

--- views/menu/verify/menu_verify.class.php
+++ views/menu/verify/menu_verify.class.php
@@ -12,13 +12,13 @@ class MenuVerify extends MenuIndexView{
 
         <!--<div id="main-header">Verification</div>-->
         
-        <br>
+       <!-- <br>
         <div style="border: 1px solid black; margin: auto; padding: 10px; text-align: center; background-color: rgba(255, 215, 0, 0.85)">
         <table>
             <tr>
                 <td style="text-align: left; vertical-align: top;">
                     <div style="background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)">
-                        <?= urldecode($message) ?>
+                        <?/*= urldecode($message) */?>
                     </div>
                     <br>
                 </td>
@@ -27,9 +27,27 @@ class MenuVerify extends MenuIndexView{
         
         <br><br><br><br>
         <div id="button-group">
-            <input class="edit-buttons" type="button" value="Back to Menu" onclick='window.location.href = "<?= BASE_URL . "/menu/index/" ?>"'>
+            <input class="edit-buttons" type="button" value="Back to Menu" onclick='window.location.href = "<?/*= BASE_URL . "/menu/index/" */?>"'>
         </div>
         </div>
+
+-->
+
+        <!-- display user  details in a form -->
+        <br><br><br><br>
+        <div style="border: 1px solid black; margin: auto; padding: 10px; text-align: center; background-color: rgba(255, 215, 0, 0.85)">
+            <?php
+            echo '<p <div style="background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)">
+                    <strong>' . $message . '</strong><br>';
+            ?>
+            <div id="button-group">
+                <input class="edit-buttons" type="button" value="Back to Menu" onclick='window.location.href = "<?= BASE_URL . "/menu/index/" ?>"'>
+            </div>
+        </div>
+        <br><br>
+
+
+
         <?php
         //display footer
         parent::displayFooter();
--- views/user/login/user_verify.class.php
+++ views/user/login/user_verify.class.php
@@ -18,7 +18,8 @@ class UserVerify extends UserIndexView{
         <div style="border: 1px solid black; margin: auto; padding: 10px; text-align: center; background-color: rgba(255, 215, 0, 0.85)">
             <input type="hidden" name="id" value="<?= $id ?>">
             <?php
-            echo '<p><strong>' . $message . '</strong><br>';
+            echo '<p <div style="background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)">
+                    <strong>' . $message . '</strong><br>';
             ?>
             <div id="button-group">
                 <input class="edit-buttons" type="button" value="View Menu" onclick='window.location.href = "<?= BASE_URL . "/menu/index/" ?>"'>
