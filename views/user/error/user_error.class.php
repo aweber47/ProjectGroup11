b1506be62c5c8aa3a4cd0f648b6c6aad76bccd68
AlexWeber <aw118@iu.edu>
2022-04-19 14:02:16 -0400

    Styled error page for users

--- views/user/error/user_error.class.php
+++ views/user/error/user_error.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /*** Author: your name*
- * Date: 4/8/2022*
+ * Date: 4/5/2022*
  * File: user_error.class.php*
  * Description: */
 class UserError extends UserIndexView
@@ -9,26 +9,24 @@ class UserError extends UserIndexView
     public function display($message)
     {
         // header
-        //parent::displayHeader("Error");
+        parent::displayHeader("Error");
         ?>
-        <div id="main-header">ERROR</div>
-        <hr>
-        <table style="width: 100%; border: none">
-            <tr>
-                <td style="text-align: left; vertical-align: top;">
-                    <h3> An error has occurred: </h3>
-                    <div style="color: red">
-                        <?= urldecode($message) ?>
-                    </div>
-                    <br>
-                </td>
-            </tr>
-        </table>
-        <br><br><br><br><hr>
-        <a href="<?= BASE_URL ?>/user/register">Back to Register Account</a>
+        <!--<div id="main-header">Error</div>-->
+        <br><br><br><br>
+        <div class="menu-error-msg">
+            <h1>An Error has Occurred.</h1>
+            <h3><?= urldecode($message) ?></h3>
+        </div>
+
+        <br><br>
+
+        <input class="return-button" type="button" value="Return to Register Form"
+               onclick="window.location.href='<?= BASE_URL ?>/user/register'">
+        <br><br>
+
         <?php
         //display page footer
-        //parent::displayFooter();
+        parent::displayFooter();
     }
 
 }
